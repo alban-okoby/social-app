429ade0361454e66bf3665ccd8748a17d65e7a49
alban-okoby <albanokoby225@gmail.com>
2022-07-06 01:53:03 +0200

    Add rightbar asstes

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import './Rightbar.css';
 
+import bithdayGif from '../../assets/image/bithday.gif';
+
 const Rightbar = () => {
   return (
-    <div className="rightbar">Rightbar</div>
+    <div className="rightbar">
+		<div className="container">
+			<img className="bithdayImg" src={bithdayGif} alt="description" />
+			<span className="bithdayText"> 		
+			<b>Kimy</b> et 2 autres amis font leurs anniversairs aujoourd'hui </span>
+		</div>
+    </div>
   )
 }
 
