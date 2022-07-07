fc2c01c40b30eccc334af7f611c341a4ff9cb730
alban-okoby <albanokoby225@gmail.com>
2022-07-07 18:32:38 +0200

    simulation des données  utilisateurs

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -7,27 +7,31 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart, faComment, faThumbsUp, faShare } from '@fortawesome/free-solid-svg-icons';
 // Components 
 import './Post.css';
+import { Users } from '../../data/data';
 
-
-const Post = (post) => {
-    console.log(post);
+const Post = ({post}) => {
   return (
     <div className='post'>
         <div className='wrapper'>
             <div className='postTop'>
+
                 <div className='postTopLeft'>
-                    <img src={profileImg} className='profile_picture' alt='description' />
-                    <b className='userName'>Cerveau Plus</b>
-                    <span className='postDate'> 3 min</span>
+                    <img src={Users.filter((u) => u.id === post?.userId)[0].profilePicture} className='profile_picture' alt={Users.filter((u) => u.id === post?.userId)[0].username} />
+                    <b className='userName'>
+                        {Users.filter((u) => u.id === post?.userId)[0].username}
+                    </b>
+                    <span className='postDate'>{post.date}</span>
                 </div>
                 <div className='postTopRight'>
                     <MoreVert />
                 </div>
+
             </div>
+
             <div className='postCenter'>
-                <p className='postText'> Le weekend est chaud bouillant ! Tout ça c'est GOD sinon personne est plus mieux que personne.  #StepByStep</p>
-                <img src='https://mdbootstrap.com/img/new/avatars/2.jpg'
-                alt="description"
+                <p className='postText'> {post.description} </p>
+                <img src={post.image}
+                alt={post.description}
                 className='object-cover h-48 w-96'
                 />
             </div>
