a7dbcc6fe52aa6af475113ac8dddba62d66044fc
alban-okoby <albanokoby225@gmail.com>
2022-07-09 04:41:59 +0200

    Login form design finishaed

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Login.css';
+import logo from '../../assets/image/logo.jpg';
 
 const Login = () => {
   return (
@@ -8,16 +9,17 @@ const Login = () => {
             <div className="loginLeft">
                 <h3 className="loginLogo">DigitalSouag</h3>
                 <span className="loginDescription">
-                    Reste connecté, pour un monde plus proche avec des potes !
+                    Reste connecté, pour un monde plus proche des potes !
                 </span>
             </div>
             <div className="loginRight">
                 <div className="loginBox">
+                    <img src={logo} className='profile_picture'/>
                     <input type="email" placeholder="Email" className="loginInput"/>
                     <input type="password" placeholder="password" className="loginInput"/>
                     <button className="loginButton">Se connecter</button>
-                    <span className="loginForgot">Mot de passe oublié </span>
-                    <button className="loginRegisterButton"> Créer un nouveau compte</button>
+                    <span className="loginForgot">Mot de passe oublié ?</span>
+                    <button className="loginRegisterButton btn-success"> Créer un nouveau compte</button>
 
                 </div>
             </div>
